2106923c5965aa1bdc857e22addfdf42f59b51da
Jade <lilyjun98@gmail.com>
2026-03-07 22:42:47 -0800

    Add base Model

---
+++ src/model/Model.php
@@ -0,0 +1,56 @@
+<?php
+require __DIR__ . '/../config/Database.php';
+
+class Model
+{
+    protected $pdo;
+    protected $table;
+
+    public function __construct()
+    {
+        $this->pdo = Database::getInstance()->getConnection();
+    }
+
+    public function all()
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$this->table}");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($data)
+    {
+        $columns = implode(',', array_keys($data));
+        $placeholders = ':' . implode(', :', array_keys($data));
+        $sql = "INSERT INTO {$this->table} ($columns) VALUES ($placeholders)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($data);
+    }
+
+    public function update($id, $data)
+    {
+        $fields = [];
+        foreach ($data as $key => $value) {
+            $fields[] = "$key = :$key";
+        }
+        $fields = implode(',', $fields);
+        $sql = "UPDATE {$this->table} SET $fields WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $data['id'] = $id;
+        return $stmt->execute($data);
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM {$this->table} WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
+}
+?>
