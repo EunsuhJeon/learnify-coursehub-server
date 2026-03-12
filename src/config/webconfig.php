2beffa6cea75e90bbaadb61d1888db24f738872f
bella-marcucci <bellamarcucci@gmail.com>
2026-03-11 17:48:21 -0700

    merged solved final

--- src/config/webconfig.php
+++ src/config/webconfig.php
@@ -1,9 +1,4 @@
 <?php
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> e035b46231c5ee2b5ae0a79091867e535a5e553b
     define("DB_SERVER","localhost");
     define("DB_USER","phpagent");
     define("DB_PASS","test123");
